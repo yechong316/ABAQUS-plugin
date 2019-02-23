f68f2e1213102c8f2b2973dd4c53352d357069fb
Ye Chong <42334254+yechong316@users.noreply.github.com>
2019-02-24 00:08:39 +0800

    Delete warning.py

--- warning.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8 
-
-# 导入ABAQUS必备的方法包
-from abaqus import *
-from abaqusConstants import *
-
-def warning(num):
-    mat_name = mdb.models['Model-1'].Material(name="composite")
-    mat_name.Density(table=((num,),))
